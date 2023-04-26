c28a4ba31d3457c33dad85f08cb4bf3f0d2106cf
Valentyn <valentunpron@gmail.com>
2023-04-26 15:03:07 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './scss/app.scss'
+import { Layout, Space } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 60,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  height: '100vh',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
+        <Layout>
+          <Sider style={siderStyle}>Sider</Sider>
+          <Layout>
+            <Header style={headerStyle}>Header</Header>
+            <Content style={contentStyle}>
+
+            </Content>
+            <Footer style={footerStyle}>Footer</Footer>
+          </Layout>
+        </Layout>
+      </Space>
     </div>
   );
 }
---
+++ src/component/Table/Table.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const CartPromocode = (): JSX.Element => {
+	return (
+
+	);
+}
